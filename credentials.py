3e2bd22c3c65be80b1a0fbb9cd9b0e929ec76ca7
Chase Davis <chasedavis@Chases-MacBook-Pro.local>
2017-11-28 00:28:16 -0500

    new files

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# Twitter login credentials -- for account @Chase_Davis_
+
+CONSUMER_KEY = 'OfFBQY83dBiacHqimEqF0DN87'
+CONSUMER_SECRET = 'bXTxwsHgo179HoTYSc2y9CWXJTqffcC5NiHWrSc5NbPCaFtiNp'
+
+ACCESS_TOKEN = '285913352-UHrszFYlJcyqYvxzaK21OGZkStAWDCEMDLBfSXNq'
+ACCESS_SECRET = 'N1hkAPPmh48w6yptpDiZXb44eMuRtfrFedwSMXhSdewGG'
